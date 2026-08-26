571c439af3c182989143d4f803ada8ce037edccf
Ankur1326 <swamiankur46@gmail.com>
2026-08-26 16:53:37 +0530

    fix alert-dialog and dialog

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -66,7 +66,6 @@ function DialogContent({
               <Button
                 variant="ghost"
                 className="absolute top-2 right-2"
-                size="icon-sm"
               />
             }
           >
